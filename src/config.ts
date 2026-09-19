a53ed48b0b240e2fde92d3d319c4bb53eaa8fb89
MahekVhora <101806979+MahekVhora@users.noreply.github.com>
2026-09-19 20:59:00 +0530

    Add TITLE_PLAIN constant and modify TITLE_ITALIC

--- src/config.ts
+++ src/config.ts
@@ -8,4 +8,5 @@ export const TMDB_API_KEY = '2923ccd110279961af46e0201491dd3b'
 
 // 2) THE BIG TITLE AT THE TOP OF THE PAGE (also becomes the browser tab title).
 //    The first part is normal text, the second part is shown in italics.
+export const TITLE_PLAIN = ''
 export const TITLE_ITALIC = "Movies and Shows"
